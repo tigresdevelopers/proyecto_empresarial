a40d6a89eb38a1d4524d5a314ef08c6e9db3cdfa
leonxandercs <alex_0446@hotmail.com>
2015-10-20 01:15:49 -0500

    x

--- Domain/src/main/java/com/network/social/domain/util/form/BaseForm.java
+++
@@ -1,9 +0,0 @@
-package com.network.social.domain.util.form;
-
-import java.io.Serializable;
-
-public class BaseForm  implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	
-}
