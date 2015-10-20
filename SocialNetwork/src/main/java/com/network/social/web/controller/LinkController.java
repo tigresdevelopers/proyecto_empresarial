f0a41a3395a0b6d06a889331cc94793eadbcf990
leonxandercs <alex_0446@hotmail.com>
2015-10-20 00:24:31 -0500

    xxxxx

---
+++ SocialNetwork/src/main/java/com/network/social/web/controller/LinkController.java
@@ -0,0 +1,38 @@
+package com.network.social.web.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
+import com.network.social.web.config.PropiedadAdmin;
+import com.network.social.web.jsf.util.Faces;
+import com.network.social.web.spring.util.AdminConfigPropiedad.LINK;
+
+@SessionScoped
+@ManagedBean(name="link")
+public class LinkController implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManagedProperty(value="#{propiedadAdmin}")
+	private PropiedadAdmin  propiedadAdmin;
+	
+	
+	public void redirect(LINK url) throws Exception {
+		System.out.println(Faces.getContextPath());
+		System.out.println(url);
+		System.out.println(propiedadAdmin.getLinkUrl(url));
+		Faces.redirect(propiedadAdmin.getLinkUrl(url));
+	}
+
+
+	public PropiedadAdmin getPropiedadAdmin() {
+		return propiedadAdmin;
+	}
+	public void setPropiedadAdmin(PropiedadAdmin propiedadAdmin) {
+		this.propiedadAdmin = propiedadAdmin;
+	}
+	
+	
+}
