3a12ce6ce6c32da1a0f1767244f3aa01968c7e35
leonxandercs <alex_0446@hotmail.com>
2015-10-22 03:13:03 -0500

    DaoUtil update

--- ServicesRestSocialNetwork/src/main/java/com/network/social/services/util/DAOUtil.java
+++ ServicesRestSocialNetwork/src/main/java/com/network/social/services/util/DAOUtil.java
@@ -6,7 +6,10 @@ public class DAOUtil {
 	
 	public static enum SP_ORACLE{
 		
-		SP_LOGIN("usplogin(?,?)");
+		SP_LOGIN("usplogin(?,?)"),
+		SP_PUBLICACION_USUARIO_TIPO(""),
+		SP_PUBLICACION_USUARIO_LISTA("")
+		;
 		
 		private final String procedure;
 		
@@ -22,7 +25,32 @@ public class DAOUtil {
 	public static enum QUERY_HQL{
 		
 		USUARIO_BY_EMAIL("usuario.email","email"),
-		ALBUM_USUARIO("album.usuario","idusuario");
+		ALBUM_USUARIO("album.usuario","idusuario"),
+		ALBUM_GRUPO("album.grupo","idgrupo"),
+		ACTIVIDAD_USUARIO("actividad.usuario"),
+		COMENTARIO_MULTIMEDIA("comentario.multimedia","idmultimedia"),
+		COMENTARIO_ALBUM("comentario.album","idalbum"),
+		COMENTARIO_PUBLICACION("comentario.publicacion","idpublicacion"),
+		COMENTARIO_COMENTARIO("comentario.comentario","idcomentario"),
+		CONTACTO_USUARIO("contacto.usuario","idusuario"),
+		ETIQUETA_MULTIMEDIA("etiqueta.multimedia","idmultimedia"),
+		GRUPO_USUARIO("grupo.usuario","idusuario"),
+		IDIOMA_USUARIO("idioma.usuario","idusuario"),
+		LIKE_MULTIMEDIA("like.multimedia","idmultimedia"),
+		LIKE_ALBUM("like.album","idalbum"),
+		LIKE_PUBLICACION("like.publicacion","idpublicacion"),
+		LIKE_COMENTARIO("like.comentario","idcomentario"),
+		LISTACONTACTO_USUARIO("listacontacto.usuario","idusuario"),
+		MULTIMEDIA_USUARIO("multimedia.usuario","idusuario"),
+		MULTIMEDIA_ALBUM("multimedia.album","idalbum"),
+		MULTIMEDIA_GRUPO("multimedia.grupo","idgrupo"),
+		MULTIMEDIA_PUBLICACION("multimedia.publicacion","idpublicacion"),
+		NOTIFICACION_USUARIO("notificacion.usuario","idusuario"),
+		PUBLICACION_USUARIO("publicacion.usuario","idusuario"),
+		PUBLICACION_GRUPO("publicacion.grupo","idgrupo"),
+		SOLICITUD_USUARIO("solicitud.usuario","idusuario")
+		;
+		
 		
 		private final String query;
 		private final String[] parameters;
